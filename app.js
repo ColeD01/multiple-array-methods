8cc859f60a2624376a23972778b16c2a5205ad49
Cole DeVore <c16devore@gmail.com>
2022-11-30 16:49:02 -0800

    added rhe bare bones begginer stuff for this assignment still need to fluch out how to alphbatize the list and make them strings that look like list elements

---
+++ app.js
@@ -0,0 +1,49 @@
+var peopleArray = [
+    {
+        firstName: "Sarah",
+        lastName: "Palin",
+        age: 47
+    },
+    {
+        firstName: "Frank",
+        lastName: "Zappa",
+        age: 12
+    },
+    {
+        firstName: "Rick",
+        lastName: "Sanchez",
+        age: 78
+    },
+    {
+        firstName: "Morty",
+        lastName: "Smith",
+        age: 29
+    },
+    {
+        firstName: "Kyle",
+        lastName: "Mooney",
+        age: 27
+    },
+    {
+        firstName: "Pasha",
+        lastName: "Datsyuk",
+        age: 13
+    },
+    {
+        firstName: "Lev",
+        lastName: "Tolstoy",
+        age: 82
+    }
+]
+
+function sortedOfAge(peopleArray) {
+    var result = peopleArray.reduce(function(final,people) {
+        if (people.age >= 18) {
+            final.push(people)
+        }
+        
+        return final
+    }, [])
+    return result
+}
+console.log(sortedOfAge(peopleArray))
